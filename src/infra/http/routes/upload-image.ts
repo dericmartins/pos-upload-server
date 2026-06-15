fe389d4f605a0deb57c51f6fe18177ff5c7d94ff
Déric Martins <dericmateus10@gmail.com>
2026-06-15 12:55:16 -0300

    add multipart upload route and integrate Swagger documentation

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,7 +1,24 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
 
 export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
-  server.post('/uploads', () => {
-    return 'ok'
-  })
+  server.post(
+    '/uploads',
+    {
+      schema: {
+        summary: 'Upload an image',
+        body: z.object({
+          name: z.string(),
+        }),
+
+        response: {
+          201: z.object({ uploadId: z.string() }),
+          409: z.object({ message: z.string() }).describe('Conflict error'),
+        },
+      },
+    },
+    async (request, reply) => {
+      return reply.status(201).send({ uploadId: 'fake-upload-id' })
+    }
+  )
 }
--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,7 +1,11 @@
 import { fastifyCors } from '@fastify/cors'
+import fastifyMultipart from '@fastify/multipart'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
 import {
   hasZodFastifySchemaValidationErrors,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
@@ -31,7 +35,23 @@ server.register(fastifyCors, {
   origin: '*',
 })
 
+server.register(fastifyMultipart)
+
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Upload Server API',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
 server.register(uploadImageRoute)
+
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('Server is running on port 3333')
